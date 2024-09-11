1024068946206d68b9c13956eba3d865d12d0a18
georga399 <georga399@gmail.com>
2024-09-11 19:50:54 +0300

    LR1

---
+++ WEB_253505_AZAROV.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WEB_253505_AZAROV.UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
