6d13d7ad4860061dbae22d39898a69684a906c11
georga399 <georga399@gmail.com>
2024-09-12 14:29:17 +0300

    Prelab 2

--- WEB_253505_AZAROV.UI/Controllers/HomeController.cs
+++ WEB_253505_AZAROV.UI/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewData["Title"] = "Лабораторная работа №2";
         return View();
     }
 
---
+++ WEB_253505_AZAROV.UI/Models/ListDemo.cs
@@ -0,0 +1,7 @@
+namespace WEB_253505_AZAROV.UI.Models;
+
+public class ListDemo 
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+}
